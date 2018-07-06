1a0fa0e05bc5dc52eabd3d039409ff7316ef1cd0
jULIAmEleshk <39305791+jULIAmEleshk@users.noreply.github.com>
2018-07-06 13:05:05 +0300

    Add files via upload

---
+++ rainbow/App.js
@@ -0,0 +1,22 @@
+﻿"use strict";
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import VotesBlock from './components/VotesBlock';
+
+let questionText='Как вы относитесь к программированию?';
+let answersArr=require('./answers.json');
+let defaultFreeAnswerText="???";
+let colorArr=['red','blue','yellow', 'green', 'violet'];
+ReactDOM.render(
+  <VotesBlock 
+    question={questionText}
+    answers={answersArr}
+    deffreeanswertext={defaultFreeAnswerText}
+    startWorkMode={1}
+		color={colorArr}
+  />
+  , document.getElementById('container') 
+);
+
---
+++ rainbow/components/ColorFrame.js
@@ -0,0 +1,24 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+class ColorFrame extends React.Component {
+	
+  static propTypes = {
+    color: PropTypes.array.isRequired,
+  };
+  
+	 render() {var c=this.props.color;;
+			var code=this.props.children;
+			for(var i = 0; i < c.length; i++){
+				code=<div style={{border:"dashed 2px "+c[i],padding:"10px"}}>{code}</div>
+			}
+    return (
+      <div>
+        {code}
+      </div>
+    );
+  }
+
+}
+
+export default ColorFrame;
