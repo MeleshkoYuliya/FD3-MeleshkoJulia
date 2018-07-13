57de3498ca9f6b181190881452e2014851e41f66
jULIAmEleshk <39305791+jULIAmEleshk@users.noreply.github.com>
2018-07-13 09:40:10 +0300

    Add files via upload

---
+++ rainbow/br2jx/components/VotesBlock.js
@@ -0,0 +1,38 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+import './VotesBlock.css';
+
+
+class VotesBlock extends React.Component {
+
+  static propTypes = {
+    question: PropTypes.string.isRequired,
+  };
+
+  
+  render() {
+		
+		var s=this.props.question;
+		var g=s.split('<br>');
+		var jsx=[];
+		g.forEach( (s, i) => {
+if (i!=0 ){
+		jsx.push(<br/>);
+	 	}
+			jsx.push(s);
+	}
+	);
+		
+    return (
+      <div className='VotesBlock'>
+			{jsx}
+      </div>
+    )
+    ;
+
+  }
+
+}
+
+export default VotesBlock;
